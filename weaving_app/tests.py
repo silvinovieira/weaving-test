19d114eb205e86a0668406564961497f97150bf7
Silvino Vieira <silvinov@gmail.com>
2023-12-21 18:37:13 +0000

    feat: take pictures

--- weaving_app/tests.py
+++ weaving_app/tests.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from hardware_controllers.cameras_controller import LightType
+from weaving_app.pictures_batch import take_pictures
 from weaving_app.surface_movement import measure_displacement, measure_velocity
 
 
@@ -17,3 +21,19 @@ def test_measure_displacement():
     velocity, time = 5.0, 2.0
     displacement = measure_displacement(velocity, time)
     assert displacement == 10.0
+
+
+def test_take_pictures(mocker):
+    mock_controller = mocker.Mock()
+    mock_tuple = (np.array([1, 2]), 3.0, 4.0, 5, np.array([6, 7]), 8.0, 9.0, 10)
+    mock_controller.collect_pictures.return_value = mock_tuple
+
+    pictures = take_pictures(mock_controller)
+
+    mock_controller.open_cameras.assert_called_once()
+    assert mock_controller.set_light_type.call_count == 2
+    assert mock_controller.trigger.call_count == 2
+    assert mock_controller.collect_pictures.call_count == 2
+    assert isinstance(pictures, dict)
+    assert set(pictures.keys()) == {LightType.GREEN, LightType.BLUE}
+    assert all(pictures[lt] == mock_tuple for lt in pictures.keys())

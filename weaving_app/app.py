bb2af15e19974bcff44b8d4f5575634284af67c9
Silvino Vieira <silvinov@gmail.com>
2023-12-22 11:17:42 +0000

    feat: add logging

--- weaving_app/app.py
+++ weaving_app/app.py
@@ -1,18 +1,25 @@
-import logging
 import queue
 
+from weaving_app.logger import configure_logger
 from weaving_app.services import PicturesBatchService, SurfaceMovementService
-
-logger = logging.getLogger()
 
 
 def main() -> None:
+    logger = configure_logger()
+
     q = queue.Queue()
-    services = SurfaceMovementService(q), PicturesBatchService(q)
-    for service in services:
-        service.daemon = True
-        service.start()
-        service.join()
+
+    surface_service = SurfaceMovementService(q, logger)
+    surface_service.daemon = True
+
+    pictures_service = PicturesBatchService(q, logger)
+    pictures_service.daemon = True
+
+    surface_service.start()
+    pictures_service.start()
+
+    surface_service.join()
+    pictures_service.join()
 
 
 if __name__ == "__main__":

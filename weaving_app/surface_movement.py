3254fae1176f055ce09f5e6075247ce70f9d5469
Silvino Vieira <silvinov@gmail.com>
2023-12-22 15:11:58 +0000

    chore: cleanup

--- weaving_app/surface_movement.py
+++
@@ -1,9 +0,0 @@
-from hardware_controllers.velocity_sensor_controller import VelocitySensorController
-
-
-def measure_velocity(controller: VelocitySensorController) -> float:
-    """Returns the velocity in centimeters per minute (cm/min)"""
-    controller.start_sensor()
-    velocity = controller.get_velocity()
-    controller.stop_sensor()
-    return velocity
